eaf027a90b4b328201b01e9371676f12c80b0fa1
Gunwoo Kim <gunwooterry@gmail.com>
2017-10-23 02:37:04 +0900

    feat(accounts): register user and school to admin

--- django/accounts/admin.py
+++ django/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, School
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('get_full_name', 'email', 'nationality', 'school', 'gender')
+    list_filter = ('gender', 'nationality', 'school')
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+    list_filter = ('country',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(School, SchoolAdmin)
