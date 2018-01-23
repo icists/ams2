3e48e06cefa3eb393f90a1e2a464986e9aa139ed
Junwoo Park <junwoopark0130@gmail.com>
2018-01-24 00:32:17 +0900

    feat: Django Rest Framework default renderer settings

--- django/ams2/settings.py
+++ django/ams2/settings.py
@@ -180,3 +180,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 SITE_ID = 1
+
+# Django Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
